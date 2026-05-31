94e20b27d907f26544c24fa285f16b884caee4d0
zht <zht@hkcx.com.cn>
2026-05-31 16:43:59 +0800

    fix: untrack yonyou-bip-dev/decompiled (generated data)

--- yonyou-bip-dev/decompiled/bip-v5/com/yonyou/iuap/coredoc/service/merchant/impl/MerchantAPIServiceImpl.java
+++
@@ -1,8 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package com.yonyou.iuap.coredoc.service.merchant.impl;
-
-public class MerchantAPIServiceImpl {
-}
-
--- yonyou-bip-dev/decompiled/bip-v5/com/yonyoucloud/upc/service/api/constant/FieldValueConstant.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package com.yonyoucloud.upc.service.api.constant;
-
-public class FieldValueConstant {
-    public static final String LEVEL = "1";
-}
-
